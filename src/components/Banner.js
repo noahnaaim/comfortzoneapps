3725e378836273eafe8f476d691fbcfac38ac3b1
noahnaaim <ybudic@yahoo.com>
2016-12-26 19:53:52 +0200

    fdfdsfsd

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,14 +4,14 @@ import $ from 'jquery'
 class Banner extends Component {
     render() {
         return (
-            <div className="banner">
+            <div className="banner" >
             </div>
         );
     }
 
     componentDidMount(){
         var embedCode = '<!-- Begin BidVertiser code --><SCRIPT SRC="http://bdv.bidvertiser.com/BidVertiser.dbm?pid=754816&bid=1859215" TYPE="text/javascript"></SCRIPT><!-- End BidVertiser code -->';
-        $('.banner').append(embedCode);
+        //$('.banner').append(embedCode);
     }
 }
 
